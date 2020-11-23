edea1bd3bba53e72e74bbc23850c7d97089ee6d3
Eurisko83200 <t.oustrain@gmail.com>
2020-11-23 19:28:40 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,602 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sam-entre",
+      "name": "SAM entrée",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4236357722889479
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0913900131562428,
+          "pitch": 0.08367143884005834,
+          "rotation": 0,
+          "target": "6-passage-coin-nuit-sam"
+        },
+        {
+          "yaw": 2.1512104927187092,
+          "pitch": 0.2331931994327583,
+          "rotation": 0,
+          "target": "7-entre-sam-et-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cuisine-derrire-cave",
+      "name": "Cuisine derrière cave",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bureau",
+      "name": "Bureau",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6639691190589865,
+          "pitch": 0.2700740832555599,
+          "rotation": 0,
+          "target": "3-coin-nuit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-coin-nuit",
+      "name": "Coin nuit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7683240134614016,
+          "pitch": 0.16215505866271407,
+          "rotation": 0,
+          "target": "6-passage-coin-nuit-sam"
+        },
+        {
+          "yaw": -0.3249215434002295,
+          "pitch": 0.0923506892312318,
+          "rotation": 0,
+          "target": "4-toilettes"
+        },
+        {
+          "yaw": 0.7042838981571169,
+          "pitch": 0.16327682965828139,
+          "rotation": 0,
+          "target": "5-salle-deau"
+        },
+        {
+          "yaw": -1.9806880707464938,
+          "pitch": 0.16105493831164708,
+          "rotation": 0,
+          "target": "2-bureau"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-toilettes",
+      "name": "Toilettes",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.13758179436138,
+          "pitch": 0.5991117192475066,
+          "rotation": 0,
+          "target": "3-coin-nuit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-salle-deau",
+      "name": "Salle d'eau",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0290197954144027,
+          "pitch": 0.6084154825227976,
+          "rotation": 0,
+          "target": "3-coin-nuit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-passage-coin-nuit-sam",
+      "name": "Passage coin nuit SAM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.335722788296316,
+          "pitch": 0.5996980952898134,
+          "rotation": 0,
+          "target": "0-sam-entre"
+        },
+        {
+          "yaw": -1.588236284050879,
+          "pitch": 0.6047029977045391,
+          "rotation": 0,
+          "target": "3-coin-nuit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-entre-sam-et-salon",
+      "name": "Entre SAM et salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.656448392933041,
+          "pitch": 0.07634306058959339,
+          "rotation": 0,
+          "target": "13-salon-bar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-salon",
+      "name": "Salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-hc",
+      "name": "HC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0837450040032195,
+          "pitch": 0.053170274513014704,
+          "rotation": 0,
+          "target": "10-passage-sam-cuisine"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-passage-sam-cuisine",
+      "name": "Passage SAM cuisine",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4017672038884665,
+          "pitch": -0.08020758651473159,
+          "rotation": 0,
+          "target": "11-cuisine-derrire-cave"
+        },
+        {
+          "yaw": 2.923512388144683,
+          "pitch": -0.01706799979473317,
+          "rotation": 0,
+          "target": "0-sam-entre"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-cuisine-derrire-cave",
+      "name": "Cuisine derrière cave",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0696675925541328,
+          "pitch": -0.009003630320950862,
+          "rotation": 0,
+          "target": "12-cuisine-devant-plaque"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-cuisine-devant-plaque",
+      "name": "Cuisine devant plaque",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.134368058304573,
+          "pitch": 0.012601905503888844,
+          "rotation": 0,
+          "target": "10-passage-sam-cuisine"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-salon-bar",
+      "name": "Salon bar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6392485659787024,
+          "pitch": 0.11093142681843382,
+          "rotation": 0,
+          "target": "9-hc"
+        },
+        {
+          "yaw": -1.7807745676701519,
+          "pitch": 0.098280588945439,
+          "rotation": 0,
+          "target": "10-passage-sam-cuisine"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-hc-fentre",
+      "name": "HC fenêtre",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
